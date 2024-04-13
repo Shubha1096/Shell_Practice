b435654916023c5ea05f5be8e6fba942a5e9116f
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 08:29:36 +0900

    Script1

--- practice-01.sh
+++ practice-01.sh
@@ -2,9 +2,18 @@
 
 # this is the comment, we need to develop a program that can do addition of 2 numbers
 
-read NUMBER1
-read NUMBER2
+#read NUMBER1
+#ead NUMBER2
 # I need to run a command inside shell to add these 2 numbers
-SUM=$((NUMBER1+NUMBER2))
+#SUM=$((NUMBER1+NUMBER2))
 
-echo "Addition of 2 numbers is: $SUM"
+#echo "Addition of 2 numbers is: $SUM"
+
+echo "Please enter your username /n"
+read  username
+echo "entered username is $username"
+
+echo "Please enter your password /n"
+
+read  password
+echo "entered password is $password"
