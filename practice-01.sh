2fababbe5872aaf6a6978ea6ec3167939ce5a9d0
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 08:24:23 +0900

    Script1

--- practice-01.sh
+++ practice-01.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-read Number1
-read Number1
 
-Sum=$((Number1+Number2))
-echo "Sum of Numbers are is $Sum"
+# this is the comment, we need to develop a program that can do addition of 2 numbers
+
+NUMBER1=$1
+NUMBER2=$2
+# I need to run a command inside shell to add these 2 numbers
+SUM=$((NUMBER1+NUMBER2))
+
+echo "Addition of 2 numbers is: $SUM"
