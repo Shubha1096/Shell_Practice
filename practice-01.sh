87400016262a3cf1d598e8f7bd8476dbc005ca22
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 08:17:00 +0900

    Script1

--- practice-01.sh
+++ practice-01.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# check if the number is greater than 10 or not
-NUMBER=$1
+echo "Hello World, I am learning DevOps with AWS"
+PERSON1=Sachin
+PERSON2=Rohit
 
-if [ $NUMBER -gt 10 ]
-then
-    echo "$NUMBER is greater than 10"
-else
-    echo "$NUMBER is not greater than 10"
-fi
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
