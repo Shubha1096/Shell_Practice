d06840a09fa306470e1ea819797f3029b03e65fe
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 08:15:23 +0900

    Script1

--- practice-01.sh
+++ practice-01.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-Number=$1
 
-if [ $Number -gt 0 ]
+# check if the number is greater than 10 or not
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
 then
-    echo "Print then"
+    echo "$NUMBER is greater than 10"
 else
-    echo "Print else"
+    echo "$NUMBER is not greater than 10"
 fi
