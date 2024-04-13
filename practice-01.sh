836eaf9ec0ac23e8f480f556a254a1ae28085362
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 08:25:52 +0900

    Script1

--- practice-01.sh
+++ practice-01.sh
@@ -2,8 +2,8 @@
 
 # this is the comment, we need to develop a program that can do addition of 2 numbers
 
-NUMBER1=$1
-NUMBER2=$2
+read NUMBER1
+read NUMBER2
 # I need to run a command inside shell to add these 2 numbers
 SUM=$((NUMBER1+NUMBER2))
 
