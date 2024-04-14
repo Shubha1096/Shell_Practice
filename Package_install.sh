2429d19c5f6b2751091045e5d1bd1c74ff93ed23
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 21:26:25 +0900

    message

--- Package_install.sh
+++ Package_install.sh
@@ -10,12 +10,12 @@ fi
    
 for i in $@
 do  
-    sudo yum list installed $i $>> /dev/null
+    yum list installed $i $>> /dev/null
     if [$? -ne 0];
     then    
         echo "$i is not installed " 
-         echo "Installing  $i ...."
-         sudo yum install $i -y &>> /dev/null
+        echo "Installing  $i ...."
+        yum install $i -y &>> /dev/null
         echo "Installation of  $i is completed"
     else 
         echo "$i is already installed "
