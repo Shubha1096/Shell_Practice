6039585571ffaf6486490d9487ac1d26efd5efd8
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 21:21:20 +0900

    message

--- Package_install.sh
+++ Package_install.sh
@@ -5,17 +5,17 @@ USER=$(id -u)
 if  [ $USER -ne 0 ];
 then    
     echo "User is not root"
-    sudo -i     
+    exit 1    
 fi 
    
 for i in $@
 do  
-    yum list installed $i $>> /dev/null
+    sudo yum list installed $i $>> /dev/null
     if [$i -ne 0];
     then    
         echo "$i is not installed " 
          echo "Installing  $i ...."
-         yum install $i -y &>> /dev/null
+         sudo yum install $i -y &>> /dev/null
         echo "Installation of  $i is completed"
     else 
         echo "$i is already installed "
