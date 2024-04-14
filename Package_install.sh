63a127ffc4de8c6ac355abc525faeddc439a43cd
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-14 21:37:03 +0900

    message

--- Package_install.sh
+++ Package_install.sh
@@ -3,15 +3,15 @@
 USER=$(id -u)
 
 
-VALIDATE (){
+# VALIDATE (){
 
-if [ $1 -ne 0 ];
-then   
-     echo "$2 is installed sucessfully" 
-else
-    echo "$2 is not installed sucessfully"   
-fi     
-}
+# if [ $1 -ne 0 ];
+# then   
+#      echo "$2 is installed sucessfully" 
+# else
+#     echo "$2 is not installed sucessfully"   
+# fi     
+# }
 
 if  [ $USER -ne 0 ];
 then    
@@ -27,7 +27,7 @@ do
         echo "$i is not installed " 
         echo "Installing  $i ...."
         yum install $i -y &>> /dev/null
-        VALIDATE "$?" "$i"
+        # VALIDATE "$?" "$i"
     else 
         echo "$i is already installed "
     fi    
