5780e506e1b03f82f636c4cc097550ea59d8684b
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-16 22:07:28 +0900

    Exercise-2

---
+++ exercise-03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# step 1:- Take user input in a string
+# step 2:- Findthe length of given string using length function
+# step 3:- Set i = length-1 and run loop till i <= 0
+# step 4:- echo the $i
+# step 5:- repeat step 3 and 4 till i==0
+# step 6:- end
+
+INPUT=DEVOPS
+LENGTH=${#INPUT}
+
+echo $LENGTH
