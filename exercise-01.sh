ce1070bfdab37cc0b36489c9e592390678b7f462
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2024-04-16 21:50:10 +0900

    Exercise-1

--- exercise-01.sh
+++ exercise-01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEXT_FILE=/home/ec2-user/Exe_File/sample.txt
-WORD_LIST=$(cat $TEXT_FILE | tr ' ' '\n' | sort | uniq -c | awk '{print $2":"$1\n}')
+WORD_LIST=$(cat $TEXT_FILE | tr ' ' '\n' | sort | uniq -c | awk '{print $2":"$1}')
 
 echo $WORD_LIST
 
